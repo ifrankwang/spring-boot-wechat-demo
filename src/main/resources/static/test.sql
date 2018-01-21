c6e8265223422abf51a6d8d79b3ed2c3e9a6401f
Frank Wang <frankwangchinese@gmail.com>
2018-01-21 10:54:10 +0800

    misc(sql): 更新sql文件

--- src/main/resources/static/test.sql
+++ src/main/resources/static/test.sql
@@ -3,10 +3,16 @@ CREATE TABLE user
 (
   id       INT AUTO_INCREMENT
     PRIMARY KEY,
-  name     VARCHAR(10) NULL,
-  username VARCHAR(10) NULL,
-  password VARCHAR(10) NULL,
+  name     VARCHAR(10) NULL
+  COMMENT '姓名',
+  username VARCHAR(10) NULL
+  COMMENT '用户名',
+  password VARCHAR(10) NULL
+  COMMENT '密码',
   gender   VARCHAR(6)  NULL
+  COMMENT '性别',
+  open_id  VARCHAR(32) NULL
+  COMMENT '微信用户id'
 )
   ENGINE = InnoDB
   COLLATE = utf8_bin;
