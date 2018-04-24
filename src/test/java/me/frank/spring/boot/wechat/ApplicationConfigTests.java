e466c21fcf88f573413416c4ec4afde0e63a61e6
Frank Wang <frankwangchinese@gmail.com>
2018-04-24 21:43:21 +0800

    misc(app): 设置测试类默认的Annotation

--- src/test/java/me/frank/spring/boot/wechat/ApplicationConfigTests.java
+++ src/test/java/me/frank/spring/boot/wechat/ApplicationConfigTests.java
@@ -7,8 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+@Rollback
+@Transactional
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationConfigTests {
